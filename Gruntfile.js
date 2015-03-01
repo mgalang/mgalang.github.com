fb8e929e51bfb0d9cf4e862b44acb5cbf7452208
Marc Galang <marchenrygalang@gmail.com>
2015-03-01 15:55:30 +0200

    Add cssmin before starting watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,5 +28,5 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // register tasks
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['cssmin', 'watch']);
 };
